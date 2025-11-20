5d17602e8d6aad7110bc072723bad31615d62ea8
Grace <gjlee@usc.edu>
2025-11-20 00:04:13 -0800

    fixed black box testing

--- app/src/test/java/com/example/chatpet/ChatPageTest.java
+++ app/src/test/java/com/example/chatpet/ChatPageTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
 import com.example.chatpet.feature4.Pet;
 
